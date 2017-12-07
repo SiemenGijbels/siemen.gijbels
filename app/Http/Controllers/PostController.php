5de90867fa6b51c50dbdeda59e0ffeff21d8fb8a
SiemenGijbels <gijbelssiemen@gmail.com>
2017-12-07 12:19:54 +0100

    zorg dat blog beschikbaar is als je niet aangemeld bent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,10 +16,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    public function __construct(){}
 
     public function getIndex() {
 
