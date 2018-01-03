e9a833f4ef15cc7f930550087cd932b7b2a384fc
SiemenGijbels <gijbelssiemen@gmail.com>
2018-01-03 15:00:11 +0100

    test commit

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,7 +15,7 @@
                      style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
                 <h2>{{ Auth::user()->name }}</h2>
                 <form enctype="multipart/form-data" action="{{ route('profile.index.changeAvatar') }}" method="POST">
-                    <label>Update Profile Image</label>
+                    <label>Update Profile Image</label> 
                     <input type="file" name="avatar">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="submit" class="pull-right btn btn-sm btn-primary">
