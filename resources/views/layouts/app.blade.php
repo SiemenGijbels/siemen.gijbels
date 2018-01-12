3f534b4033e4c5a1ba5a95e4cdca9d2b32cf7e44
SiemenGijbels <gijbelssiemen@gmail.com>
2018-01-13 00:34:50 +0100

    added tracking and favicon / mask icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-86512043-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-86512043-1');
+
+    </script>
+
     <title>@yield('page_title')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,6 +28,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/png" href="https://www.siemengijbels.com/img/thinkingface.png">
+    <link rel="mask-icon" href="https://www.siemengijbels.com/img/thinkingemoji.svg" viewbox="0 0 16 16" color="#8A2BE2">
     <script src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
     <script src="{{ asset('js/bootstrap-hover-dropdown.js') }}" type="text/javascript"></script>
     @yield('head_scripts')
